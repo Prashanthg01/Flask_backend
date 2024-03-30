1b4fe8572b199d83835af272a4a85b1d07506f3e
Prashanthg01 <prashanthprashanth8585@gmail.com>
2024-03-31 00:04:37 +0530

    Cleaned template and static files

--- reviewcruncher/admin/views/__init__.py
+++ reviewcruncher/admin/views/__init__.py
@@ -2,5 +2,4 @@ from flask import Blueprint
 
 myadmin = Blueprint('myadmin', __name__)
 
-from reviewcruncher.admin.views import registration
-from reviewcruncher.admin.views import upload_product
+from reviewcruncher.admin.views import views
